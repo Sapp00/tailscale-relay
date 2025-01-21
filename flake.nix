390753240c5e26e60d58b837eb2867433112db83
Maurice <info@heymann.dev>
2025-01-21 17:39:19 +0100

    added disko

--- flake.nix
+++ flake.nix
@@ -3,25 +3,33 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05"; # Nixpkgs version
-    nixosModules.url = "github:nix-community/nixos-modules"; # Optional for custom modules
+    impermanence.url = "github:nix-community/impermanence";
 
-    # Tailscale flake input
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     tailscale = {
-      url = "github:tailscale/nix-tailscale";
+      url = "github:tailscale/tailscale";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, tailscale, ... }: {
+  outputs = { self, nixpkgs, tailscale, disko, ... }@inputs: {
     nixosConfigurations = {
       relay-node = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux"; # Choose the architecture for your node
+        specialArgs.inputs = inputs;
         modules = [
+          disko.nixosModules.disko
+          ./disko-config.nix
+          {
+            _module.args.disks = [ "/dev/sda" ];
+          }
           ./configuration.nix  # The relay node configuration
-          tailscale.nixosModule  # Import the Tailscale NixOS module
+          tailscale.nixosModules.tailscale  # Import the Tailscale NixOS module
         ];
-
-        specialArgs = { inherit system; };
       };
     };
   };
